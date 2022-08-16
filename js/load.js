297e36312ce6d729ace3031985523561df1496c5
Leo Eletzhofer <leo.eletzhofer@icloud.com>
2022-08-16 14:16:20 +0200

    walking

--- js/load.js
+++ js/load.js
@@ -1,11 +1,12 @@
 class Load {
     preload(){
         this.load.image('background', 'assets/background.png');
-        this.load.image('player', 'assets/player.png');
-        this.load.image('coin', 'assets/coin.png');
-        this.load.image('enemy', 'assets/enemy.png');
+        this.load.image('player', 'assets/playerR.png');
+        this.load.image('cola', 'assets/cola.png');
+        this.load.image('slime', 'assets/slime.png');
         this.load.image('wallV', 'assets/wallVertical.png');
         this.load.image('wallH', 'assets/wallHorizontal.png');
+        this.load.image('gun', 'assets/gun.png');
         let loadLabel = this.add.text(250,170, 'loading',
             {font: '30px Arial', fill: '#fff'});
         loadLabel.setOrigin(0.5,0.5);
